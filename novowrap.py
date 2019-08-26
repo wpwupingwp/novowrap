0a6e484bc5261de5a7decb60bebf8a7be7783f20
Workstation <wpwupingwp@outlook.com>
2019-08-26 17:07:51 +0800

    clean novowrap

--- novowrap.py
+++ novowrap.py
@@ -14,7 +14,7 @@ from tempfile import TemporaryDirectory
 import argparse
 import logging
 
-from utils import rotate_seq
+from utils import rotate_seq, get_full_taxon
 
 
 # temporary directory
@@ -50,67 +50,6 @@ def parse_args():
     # arg.add_argument('-split', default=1_000_000,
     #                  help='reads to use (million), set to 0 to skip split')
     return arg.parse_args()
-
-
-def get_full_taxon(taxon):
-    """
-    Get full lineage of given taxon
-    Return lineage list, only contains Kingdom, Phylum, Class, Order, Family,
-    Genus, Species.
-    Arg:
-        taxon(str): given taxon name, could be common name, use quotation mark
-        if necessary
-    Return:
-        lineage(list): lineage list, from higher to lower rank
-    """
-    split = taxon.split(' ')
-    if len(split) >= 2 and split[0][0].isupper() and split[0][1].islower():
-        name = ' '.join(split[0:2])
-    else:
-        name = split[0]
-    Entrez.email = 'guest@example.org'
-    search = Entrez.read(Entrez.esearch(db='taxonomy', term=f'"{name}"'))
-    if search['Count'] == '0':
-        if ' ' not in name:
-            log.critical(f'Cannot find {name} in NCBI Taxonomy.')
-            return None
-        if ' ' in name:
-            name = split[0]
-            sleep(0.5)
-            search = Entrez.read(Entrez.esearch(db='taxonomy',
-                                                term=f'"{name}"'))
-            if search['Count'] == '0':
-                log.critical(f'Cannot find {name} in NCBI Taxonomy.')
-                return None
-    taxon_id = search['IdList'][0]
-    record = Entrez.read(Entrez.efetch(db='taxonomy', id=taxon_id))[0]
-    names = [i['ScientificName'] for i in record['LineageEx']]
-    full_lineage = {i['Rank']: i['ScientificName'] for i in
-                    record['LineageEx']}
-    full_lineage[record['Rank']] = record['ScientificName']
-    if 'kingdom' not in full_lineage:
-        full_lineage['kingdom'] = full_lineage['superkingdom']
-    if ('class' not in full_lineage and 'order' in full_lineage):
-        last_phyta = ''
-        for i in names[::-1]:
-            if i.endswith('phyta'):
-                last_phyta = i
-                break
-        # virus do not have phylum?
-        phylum = full_lineage.get('phylum', None)
-        if last_phyta != phylum:
-            full_lineage['class'] = last_phyta
-    target = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus',
-              'species']
-    lineage = []
-    for i in target:
-        lineage.append(full_lineage.get(i, ''))
-    if ' ' in lineage[-1]:
-        lineage[-1] = lineage[-1].split(' ')[-1]
-    # species name contains genus
-    if lineage[-1] != '' and lineage[-2] != '':
-        lineage[-1] = f'"{lineage[-2]} {lineage[-1]}"'
-    return lineage
 
 
 def get_seq(taxon, output, gene=None):
@@ -223,103 +162,6 @@ Use Quality Scores    = no
     with open(config_file, 'w') as out:
         out.write(config)
     return config_file
-
-
-def repeat_and_reverse(fasta, taxon):
-    """
-    Duplicate sequence to get full length of IR which may locate in start or
-    end of sequences that BLAST cannot get whole length.
-    Detect direction of sequence by BLASTing rbcL, reverse if needed.
-    Replace illegal character in sequence with "N".
-    Args:
-        fasta(Path): fasta filename
-        taxon(Path): taxonomy of given fasta
-    Return:
-        new_fasta(Path): new_fasta's name
-    """
-    strand = {}
-    # get rbcL reference
-    rbcL = Path(TMP.name) / f'{taxon}-rbcL_ref' / 'by-gene' / 'rbcL.fasta'
-    if rbcL.exists():
-        pass
-    else:
-        rbcL, _ = next(get_seq(taxon, Path(TMP.name), 'rbcL'))
-
-    rbcL_blast = blast(rbcL, fasta)
-    for query in parse_blast_tab(rbcL_blast):
-        for hit in query:
-            (qseqid, sseqid, qseq, sseq, qlen, pident, gapopen,
-             qstart, qend, sstart, send) = hit
-            if qstart < qend and sstart < send:
-                strand[sseqid] = '+'
-            else:
-                strand[sseqid] = '-'
-    new_fasta = fasta.with_suffix('.new')
-    new = []
-    for i in SeqIO.parse(fasta, 'fasta'):
-        n_star = str(i.seq).count('*')
-        if n_star != 0:
-            log.info(f'Remove {n_star} "*" in {i.description}.')
-            i.seq = Seq(str(i.seq).replace('*', ''))
-        i.seq = i.seq + i.seq
-        # negative strand or not found by blast
-        if strand.get(i.id, '-') == '-':
-            # reverse complement instead of reverse
-            old_name = i.description
-            i = i.reverse_complement()
-            i.id = old_name
-            i.description = ''
-            log.warning(f'Found reversed sequence in "{i.id}". Reverse back.')
-        new.append(i)
-    SeqIO.write(new, new_fasta, 'fasta')
-    return new_fasta
-
-
-def blast(query, target):
-    """
-    Use simple BLAST with special output format.
-    Args:
-        query(Path): query filename
-        target(Path): target filename
-    Return:
-        blast_out(Path): blast result filename
-    """
-    FMT = 'qseqid sseqid qseq sseq qlen pident gapopen qstart qend sstart send'
-    blast_out = query.with_suffix('.blast')
-    # use blastn -subject instead of makeblastdb
-    blast = run(f'blastn -query {query} -subject {target} -outfmt "7 {FMT}" '
-                f'-out {blast_out}', shell=True, stdout=NULL, stderr=NULL)
-    # remove makeblastdb result
-    if blast.returncode != 0:
-        log.critical('Cannot run BLAST.')
-        exit(-1)
-    return blast_out
-
-
-def parse_blast_tab(filename):
-    """
-    Parse BLAST result (tab format).
-    Return [qseqid, sseqid, qseq, sseq, qlen, pident, gapopen, qstart, qend, sstart,
-    send]
-    Arg:
-        filename(Path): blast result file
-    Return:
-        line(list): parsed result
-    """
-    query = []
-    with open(filename, 'r', encoding='utf-8') as raw:
-        for line in raw:
-            if line.startswith('# BLAST'):
-                if query:
-                    yield query
-                query.clear()
-            elif line.startswith('#'):
-                pass
-            else:
-                line = line.strip().split('\t')
-                line[4:] = list(map(float, line[4:]))
-                line[4:] = list(map(int, line[4:]))
-                query.append(line)
 
 
 def txt_to_fasta(old):

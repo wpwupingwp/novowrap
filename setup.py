f3c01a785d5113456fa7440d88e4245e09c78cfe
Ping Wu <wpwupingwp@outlook.com>
2020-03-04 20:40:16 +0800

    limit version >=3.6

--- setup.py
+++ setup.py
@@ -18,15 +18,19 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     name='novowrap',
     packages=setuptools.find_packages(),
+    # f-string and Path-like
+    python_requires='>=3.6',
     url='https://github.com/wpwupingwp/novowrap',
     version='0.95',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ],
 )

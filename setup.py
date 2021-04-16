0db056460f41c780439bf2a9f50cedd9da00b834
Workstation <wpwupingwp@outlook.com>
2021-04-16 18:03:32 +0800

    merge offline

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as _:
 setuptools.setup(
     author='Ping Wu',
     author_email='wpwupingwp@outlook.com',
-    description='Assembly Chloroplast Genome',
+    description='Assembly Plastid Genome',
     install_requires=requires,
     license='GNU AGPL v3',
     long_description=long_description,
@@ -19,18 +19,15 @@ setuptools.setup(
     name='novowrap',
     packages=setuptools.find_packages(),
     # f-string and Path-like
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     url='https://github.com/wpwupingwp/novowrap',
-    version='0.986',
+    version='1.1',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ],
 )
